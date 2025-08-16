a83da1228cc6f999071fd1c1c87bf2bc24e6922d
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 18:45:43 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HistoryModule } from './history/history.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { history } from './history/entities/history.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    HistoryModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      database: 'postman_clone',
+      username: 'root',
+      password: '',
+      synchronize: true,
+      entities: [history],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
