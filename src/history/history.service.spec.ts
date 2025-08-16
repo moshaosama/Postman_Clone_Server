9db79e9a25be607d3051ef4973e023d1f8c5c8cd
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 19:13:50 +0300

    Update history.service.spec.ts

--- src/history/history.service.spec.ts
+++ src/history/history.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HistoryService } from './history.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { history } from './entities/history.entity';
 
 describe('HistoryService', () => {
   let service: HistoryService;
 
+  const mockRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HistoryService],
+      providers: [
+        HistoryService,
+        {
+          provide: getRepositoryToken(history),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<HistoryService>(HistoryService);
