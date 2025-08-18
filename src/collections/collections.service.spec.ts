612aa6c0dff0a130c5df7db42677b1a6f533e372
moshaosama <mohamedOSFekry@gmail.com>
2025-08-18 03:27:16 +0300

    Update collections.service.spec.ts

--- src/collections/collections.service.spec.ts
+++ src/collections/collections.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsService } from './collections.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { collection } from './entities/collection.entity';
 
 describe('CollectionsService', () => {
   let service: CollectionsService;
 
+  const mockRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CollectionsService],
+      providers: [
+        CollectionsService,
+        {
+          provide: getRepositoryToken(collection),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<CollectionsService>(CollectionsService);
