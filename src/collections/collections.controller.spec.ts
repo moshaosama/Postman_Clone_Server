a8ecb55027c1430924959c9495da718243f0b842
moshaosama <mohamedOSFekry@gmail.com>
2025-08-18 03:27:15 +0300

    Update collections.controller.spec.ts

--- src/collections/collections.controller.spec.ts
+++ src/collections/collections.controller.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
+import { CreateCollectionDto } from './dto/create-collection.dto';
 
 describe('CollectionsController', () => {
   let controller: CollectionsController;
+
+  const MockFunctions = {
+    CreateHistory: jest.fn(),
+    GetCollections: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,7 +20,34 @@ describe('CollectionsController', () => {
     controller = module.get<CollectionsController>(CollectionsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be create a new collections', async () => {
+    const newcollection: CreateCollectionDto = {
+      history_id: 1,
+      title: 'Postman_clone',
+    };
+
+    const resolvedData = {
+      statusbar: 'success',
+      message: 'created successfully',
+    };
+
+    await MockFunctions.CreateHistory.mockResolvedValue(resolvedData);
+
+    const result = await controller.createHistory(newcollection);
+    expect(result).toEqual(resolvedData);
+  });
+
+  it('should get all collections in db', async () => {
+    const resolvedData = [
+      {
+        title: 'PostMan_Clone',
+        history_id: 1,
+      },
+    ];
+
+    await MockFunctions.GetCollections.mockResolvedValue(resolvedData);
+
+    const result = await controller.getCollections();
+    expect(result).toEqual(resolvedData);
   });
 });
