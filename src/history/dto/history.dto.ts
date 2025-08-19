d7428fc6c62424329975cc0547c8b1d5defd00c9
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 20:10:46 +0300

    Update history.dto.ts

--- src/history/dto/history.dto.ts
+++ src/history/dto/history.dto.ts
@@ -1,7 +1,11 @@
+import { collection } from 'src/collections/entities/collection.entity';
+
 export class HistoryDto {
   id: number;
 
   method: string;
 
   url: string;
+
+  collection_id: collection | number;
 }
