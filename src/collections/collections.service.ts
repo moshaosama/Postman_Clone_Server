81051bd3e8cdc2293b65274a3378a889e2029535
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 20:10:45 +0300

    Update collections.service.ts

--- src/collections/collections.service.ts
+++ src/collections/collections.service.ts
@@ -13,22 +13,17 @@ export class CollectionsService {
   ) {}
 
   async CreateHistory(createHistoryDTO: CreateCollectionDto) {
-    if (
-      !createHistoryDTO ||
-      !createHistoryDTO.history_id ||
-      !createHistoryDTO.title
-    ) {
+    if (!createHistoryDTO || !createHistoryDTO.title) {
       throw new HttpException(
         { status: HttpStatus.BAD_REQUEST, error: 'Invalid data' },
         HttpStatus.BAD_REQUEST,
       );
     }
-    const newCollection = {
+    const newCollection = this.collectionModel.create({
       title: createHistoryDTO.title,
-      history_id: createHistoryDTO.history_id,
-    };
+    });
 
-    return this.collectionModel.create(newCollection);
+    return this.collectionModel.save(newCollection);
   }
 
   async GetCollections() {
