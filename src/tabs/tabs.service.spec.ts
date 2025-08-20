e5ab52e5d4ac965f34e4e96a51290548904f45ae
moshaosama <mohamedOSFekry@gmail.com>
2025-08-20 22:18:48 +0300

    Update tabs.service.spec.ts

--- src/tabs/tabs.service.spec.ts
+++ src/tabs/tabs.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TabsService } from './tabs.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { tabs } from './entities/tab.entity';
 
 describe('TabsService', () => {
   let service: TabsService;
+  let mockRepository;
 
   beforeEach(async () => {
+    mockRepository = {
+      find: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TabsService],
+      providers: [
+        TabsService,
+        {
+          provide: getRepositoryToken(tabs),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TabsService>(TabsService);
